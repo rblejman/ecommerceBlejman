9a86aae81e8f92afacd20fb3db4e4c8bdd0469dc
rodrigo blejman <rodrigo.blej@gmail.com>
2022-03-20 00:27:29 -0300

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Brand</h1>
+      <nav>
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </nav>
+    </header>
+  );
+};
+export default Header;
