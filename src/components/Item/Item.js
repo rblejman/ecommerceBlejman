fc39de9ce9af536bc8049d7784230a8b50ba1b6a
rodrigo blejman <rodrigo.blej@gmail.com>
2022-04-03 16:39:21 -0300

    itemlist and item ok

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,7 +2,7 @@ import "./Item.css";
 export const Item = ({ product }) => {
   console.log("hola soy Item", product);
   return (
-    <div>
+    <div className="card">
       <img src={product.pictureURL} alt="" />
       <h1>{product.title}</h1>
       <h5>{product.description}</h5>
