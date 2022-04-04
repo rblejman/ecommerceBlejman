f00c8e04d70ac047117565f6e1da7b616b17b466
rodrigo blejman <rodrigo.blej@gmail.com>
2022-04-04 17:17:39 -0300

    initializing itemDetail and fetching API

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,7 +1,10 @@
-export const ItemDetail = () => {
+export const ItemDetail = ({ product }) => {
   return (
     <div>
       <h2>ItemDetail</h2>
+      <img src={product.image} alt="" />
+      <h2>{product.title}</h2>
+      <p>Price: ${product.price}</p>
     </div>
   );
 };
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
 import { ItemDetail } from "../ItemDetail/ItemDetail.js";
 export const ItemDetailContainer = () => {
-  return <ItemDetail />;
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    console.log("useEffect ItemDetailContainer se ejecuto");
+    fetch("https://fakestoreapi.com/products/1")
+      .then((data) => data.json())
+      .then((item) => {
+        setProduct(item);
+      })
+      .catch(() => {
+        console.log("error ItemDetailContainer");
+      });
+  }, []);
+
+  return <ItemDetail product={product} key={product.id} />;
 };
