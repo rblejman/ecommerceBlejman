049bea4d6767fdd5f446c257fccbeb3a08e7900b
rodrigo blejman <rodrigo.blej@gmail.com>
2022-04-23 18:44:09 -0300

    firebase added to ILC. working

--- src/components/ItemListContainer/Index.jsx
+++ src/components/ItemListContainer/Index.jsx
@@ -1,39 +1,60 @@
 import React, { useEffect, useState } from "react";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { useParams } from "react-router-dom";
 import "./ItemListContainer.css";
 import { ItemList } from "../ItemList/Index";
 import CircularProgress from "@mui/material/CircularProgress";
-import { useParams } from "react-router-dom";
 import { initialProducts } from "../mock/InitialProducts";
+//firebase
+import { db } from "../../firebase/firebase";
 
 const ItemListContainer = ({ greeting }) => {
-  // let stock = 5;
-  // let initial = 1;
-
   const [products, setProducts] = useState([]);
-  const fetchProduct = new Promise((res, rej) => {
-    setTimeout(() => {
-      res(initialProducts);
-    }, 1000);
-  });
+  // const fetchProduct = new Promise((res, rej) => {
+  //   setTimeout(() => {
+  //     res(initialProducts);
+  //   }, 1000);
+  // });
 
+  // recibo la categoria por useparams. Si esta definido lo uso para filtrar, sino traigo todo
   const { categoryId } = useParams();
-  console.log(categoryId);
 
   useEffect(() => {
-    fetchProduct
-      .then((fetchedProducts) => {
-        if (categoryId != undefined) {
-          const filteredProds = fetchedProducts.filter(
-            (product) => product.category == categoryId
-          );
-          setProducts(filteredProds);
-        } else {
-          setProducts(fetchedProducts);
-        }
+    const productsCollection = collection(db, "products");
+    const q = query(
+      productsCollection,
+      where("category", "==", `${categoryId}`)
+    );
+    getDocs(categoryId ? q : productsCollection)
+      .then((result) => {
+        const docs = result.docs;
+        const prodList = docs.map((element) => {
+          const id = element.id;
+          const product = { id, ...element.data() };
+          return product;
+        });
+
+        console.log(prodList);
+        setProducts(prodList);
       })
-      .catch(() => {
-        console.log("todo mal");
+      .catch((error) => {
+        console.log(error);
       });
+
+    // fetchProduct
+    //   .then((fetchedProducts) => {
+    //     if (categoryId != undefined) {
+    //       const filteredProds = fetchedProducts.filter(
+    //         (product) => product.category == categoryId
+    //       );
+    //       setProducts(filteredProds);
+    //     } else {
+    //       setProducts(fetchedProducts);
+    //     }
+    //   })
+    //   .catch(() => {
+    //     console.log("todo mal");
+    //   });
   }, [categoryId]);
 
   return (
