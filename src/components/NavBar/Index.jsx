3b987fed4a6e3e423fe709136c42655edbd710c4
rodrigo blejman <rodrigo.blej@gmail.com>
2022-05-06 20:22:33 -0300

    new style for responsive navbar

--- src/components/NavBar/Index.jsx
+++ src/components/NavBar/Index.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import "./NavBar.css";
+import React, { useState } from "react";
+import "./styles.css";
 import CartWidget from "../CartWidget/Index";
 import { Link } from "react-router-dom";
+import logo from "../../assets/imgs/light-logo.png";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import ClearRoundedIcon from "@mui/icons-material/ClearRounded";
 
 const categories = [
   { name: "Techo", route: "/categories/techo", id: 1 },
@@ -11,25 +14,43 @@ const categories = [
 ];
 
 const NavBar = () => {
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <header>
-      <Link to="/">
-        <h1>Brand</h1>
-      </Link>
+      <nav className="navbar">
+        <Link to="/" className="navbar__logo">
+          <img src={logo} alt="Lumen Company Logo" className="logo" />
+        </Link>
 
-      <nav>
-        {categories.map((element) => {
-          return (
-            <Link to={element.route} key={element.id}>
-              {element.name}
-            </Link>
-          );
-        })}
+        <ul className={clicked ? "navbar__menu active" : "navbar__menu"}>
+          {categories.map((element) => {
+            return (
+              <li>
+                <Link to={element.route} key={element.id}>
+                  {element.name}
+                </Link>
+              </li>
+            );
+          })}
+          <Link to="/cart" className="navbar__widget">
+            <CartWidget />
+          </Link>
+        </ul>
+
+        {clicked ? (
+          <div onClick={handleClick} className="navbar__icon">
+            <ClearRoundedIcon />
+          </div>
+        ) : (
+          <div onClick={handleClick} className="navbar__icon">
+            <MenuRoundedIcon />
+          </div>
+        )}
       </nav>
-
-      <Link to="/cart">
-        <CartWidget />
-      </Link>
     </header>
   );
 };
